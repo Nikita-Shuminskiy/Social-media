2fac9181cdbdf716375c939856091e2076bdd895
Nick <shuminskiy.nik@mail.ru>
2021-09-10 22:11:50 +0300

    Updates

--- static/js/6.201979c3.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-samyrai"]=this["webpackJsonpreact-samyrai"]||[]).push([[6],{294:function(t,a,n){},296:function(t,a,n){"use strict";n.r(a);n(0);var c=n(294),s=n.n(c),e=n(1);a.default=function(t){return Object(e.jsx)("div",{className:s.a.content,children:"12121212121"})}}}]);
-//# sourceMappingURL=6.201979c3.chunk.js.map
