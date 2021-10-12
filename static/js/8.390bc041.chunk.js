6e4a63a5f1d7b65cd58fa1b127d4bff44a80896b
Nick <shuminskiy.nik@mail.ru>
2021-10-12 19:42:46 +0300

    Updates

--- static/js/8.390bc041.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-samyrai"]=this["webpackJsonpreact-samyrai"]||[]).push([[8],{297:function(t,a,n){},334:function(t,a,n){"use strict";n.r(a);n(0);var c=n(297),s=n.n(c),e=n(2);a.default=function(t){return Object(e.jsx)("div",{className:s.a.content,children:"12121212121"})}}}]);
-//# sourceMappingURL=8.390bc041.chunk.js.map
---
+++ static/js/8.4f753feb.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-samyrai"]=this["webpackJsonpreact-samyrai"]||[]).push([[8],{370:function(t,a,n){},393:function(t,a,n){"use strict";n.r(a);n(0);var c=n(370),s=n.n(c),e=n(3);a.default=function(t){return Object(e.jsx)("div",{className:s.a.content,children:"12121212121"})}}}]);
+//# sourceMappingURL=8.4f753feb.chunk.js.map
