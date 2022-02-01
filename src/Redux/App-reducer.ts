40c54feba4d8cbc25c40ab2239362ce2767be8f4
nick-gansta <shuminskiy.nik@mail.ru>
2022-02-01 21:38:44 +0300

    test

--- src/Redux/App-reducer.ts
+++ src/Redux/App-reducer.ts
@@ -1,37 +1,36 @@
-import { ActionsTypes, AppDispatchType } from './Redux_Store'
-import { getUserAutMeThunk } from './Auth_Reducer';
 import { Dispatch } from 'redux';
-
+import { getUserAutMeThunk } from './Auth_Reducer';
+import { ActionsTypes } from './Redux_Store'
 
 type initType = {
-    initialized: boolean
+  initialized: boolean
 }
 const initialState: initType = {
-    initialized: false
+  initialized: false,
 }
 
 export function AppReducer(state = initialState, action: ActionsTypes): initType {
-    switch (action.type) {
-        case 'APP/SET-INITIALIZED': {
-            return {
-                ...state,
-                initialized: true
-            }
-        }
-        default:
-            return state
+  switch (action.type) {
+    case 'APP/SET-INITIALIZED': {
+      return {
+        ...state,
+        initialized: true,
+      }
     }
+    default:
+      return state
+  }
 }
 
 export const initializedSuccess = () => ({type: 'APP/SET-INITIALIZED'} as const)
 
 export const initializeAppThunk = () => (dispatch: Dispatch) => {
-    // @ts-ignore
-    let promise = dispatch(getUserAutMeThunk())
-    Promise.all([promise])
-        .then(() => {
-            dispatch(initializedSuccess())
-        })
+  // @ts-ignore
+  let promise = dispatch(getUserAutMeThunk())
+  Promise.all([promise])
+  .then(() => {
+    dispatch(initializedSuccess())
+  })
 
 }
 
--- src/components/Users/Users/Users.tsx
+++ src/components/Users/Users/Users.tsx
@@ -1,46 +1,52 @@
 import React from 'react';
-import s from './Users.module.css';
-import { User } from '../User/User';
-import { Paginator } from '../../Common/Paginator/Paginator';
 import { ApiUserType } from '../../../Api/Api';
-
+import { Paginator } from '../../Common/Paginator/Paginator';
+import { User } from '../User/User';
+import s from './Users.module.css';
 
 export type UserComponentType = {
-    pageClickChange: (page: number) => void
-    users: ApiUserType[]
-    pageSize: number
-    currentPage: number
-    totalCount: number
-    disabledInProgressUser: Array<number>
-    followThunk: (id: number) => void
-    unfollowThunk: (id: number) => void
+  pageClickChange: (page: number) => void
+  users: ApiUserType[]
+  pageSize: number
+  currentPage: number
+  totalCount: number
+  disabledInProgressUser: Array<number>
+  followThunk: (id: number) => void
+  unfollowThunk: (id: number) => void
 }
 
-
 export const Users = (props: UserComponentType) => {
+  const array1 = [1, 2, 3, 4, 5, 6,1,2,3,4]
 
+  const mapArr = (arr: number[]) => {
+    let new_arr = []
+    for (let item of arr) {
+      new_arr.push(item)
+    }
+    return <div>{new_arr}</div>
+  }
 
-
-    return (<div className={s.container}>
-            <div >
-                <Paginator pageClickChange={props.pageClickChange}
-                           currentPage={props.currentPage}
-                           pageSize={props.pageSize}
-                           totalCount={props.totalCount}/>
-            </div>
-            {props.users.map(u => {
-                    return (<User key={u.id}
-                                  idUser={u.id}
-                                  smallImgUser={u.photos.small}
-                                  disabledInProgressUser={props.disabledInProgressUser}
-                                  followed={u.followed}
-                                  unfollowThunk={props.unfollowThunk}
-                                  followThunk={props.followThunk}
-                                  nameUser={u.name}
-                                  status={u.status}/>)
-                }
-            )
-            }
-        </div>
-    )
+  return (<div className={s.container}>
+      {mapArr(array1)}
+      {props.users.map(u => {
+          return (<User key={u.id}
+                        idUser={u.id}
+                        smallImgUser={u.photos.small}
+                        disabledInProgressUser={props.disabledInProgressUser}
+                        followed={u.followed}
+                        unfollowThunk={props.unfollowThunk}
+                        followThunk={props.followThunk}
+                        nameUser={u.name}
+                        status={u.status}/>)
+        },
+      )
+      }
+      <div>
+        <Paginator pageClickChange={props.pageClickChange}
+                   currentPage={props.currentPage}
+                   pageSize={props.pageSize}
+                   totalCount={props.totalCount}/>
+      </div>
+    </div>
+  )
 }
